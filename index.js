2c26c75c6c969c6062dacd951777796c9770e640
Tyler Scott <off.nadir.insight@gmail.com>
2021-02-13 15:10:46 -0600

    single round with error for invalid entry

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ COMPLETE
 const arr = ['rock', 'paper', 'scissors'];
 
 // return random element from array 'arr'
-function computerPlay(arr) {
+function computerPlay() {
     const ele = getRandomInt(arr.length);
     return arr[ele];
 }
@@ -26,18 +26,45 @@ function getRandomInt(max) {
 /*
 FOCUS:
 // play a round of rock paper scissors
-    // ask user "provide your move" and store the answer in "playerSelection"
+    // x ask user "provide your move" and store the answer in "playerSelection"
         // x make case insensitive (rock, ROCK, rOcK, etc)
         // return error message if not valid selection
-    // generate computer play as "computerSelection"
+            // 
+    // x generate computer play as "computerSelection"
     // compare playerSelection to computerSelection to determine winner
-    // return the outcome
+        // x win/lose scenarios
+        // x tie
+        // invalid
+    // x return the outcome
 */
+
+const errorMsg = [
+    "You win! Rock beats scissors",
+    "You win! Scissors beats paper",
+    "You win! Paper beats rock",
+    "You lose! Rock beats scissors",
+    "You lose! Scissors beats paper",
+    "You lose! Paper beats rock",
+    "It's a tie!"
+]
 
 function singleRoundRPS(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        alert("You win! Rock beats scissors!")
-    } else {
+        alert(errorMsg[0])
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        alert(errorMsg[5])
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        alert(errorMsg[2]);
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        alert(errorMsg[4]);
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        alert(errorMsg[3]);
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        alert(errorMsg[1]);
+    } else if (playerSelection === computerSelection) {
+        alert(errorMsg[6]);
+    } 
+    else {
         alert(`player: ${playerSelection} comp: ${computerSelection}`)
     }
 }
@@ -45,8 +72,26 @@ function singleRoundRPS(playerSelection, computerSelection) {
 // prompt & return player's move in lowercase format
 function askPlayer() {
     let play = prompt('Rock, Paper, or Scissors?')
-    return play.toLowerCase();
+    play = play.toLowerCase();
+    if (play === "rock" || play === "paper" || play === "scissors") {
+        return play;
+    } else {
+        console.error("invalid selection");
+    }
+}
+
+function getValidFmPlayer() {
+    let reply = askPlayer();
+    switch (reply) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            return reply;
+            break;
+        default:
+            return "invalid selection";
+    }
 }
 
 
-singleRoundRPS(askPlayer(), "scissors");
+singleRoundRPS(askPlayer(), computerPlay());
